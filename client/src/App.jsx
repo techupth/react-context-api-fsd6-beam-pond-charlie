0da5b91f9a783cfb9cdd5ff5d5ae2416e8a06f7f
CharlieHii <charlie_chaper@hotmail.com>
2024-06-14 14:19:18 +0700

    feat(minimum): add user data context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createContext } from "react";
 import "./App.css";
+
+export const UserDataContext = createContext();
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
@@ -13,12 +16,14 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
+      <UserDataContext.Provider value={userData}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,12 @@
+import { UserDataContext } from "../App";
+import { useContext } from "react";
+
 function AppHeader() {
+  const { username } = useContext(UserDataContext);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {username}</h2>
     </div>
   );
 }
